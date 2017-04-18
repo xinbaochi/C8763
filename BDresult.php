fd71d249c4426a64e4601a43f550250660d9aa68
xinbaochiliuzan <lollinlol777@gmail.com>
2017-04-18 19:57:32 +0800

    Add files via upload

---
+++ BDresult.php
@@ -0,0 +1,83 @@
+<html>
+
+<head>
+
+<title>表單確認</title>
+
+</head>
+
+<body background="frog.jpg" >
+<center><h1><font face="verdana" size="80">【2087重力可可鼻地營】報名資料確認</font></h1><br>
+<center><img src="photo.jpg" width="value" height="value"/>
+<?php
+
+$Name=$_GET["Name"];
+$gender=$_GET["dick"];
+$birth=$_GET["birth"];
+$id=$_GET["id"];
+$h=$_GET["hnum"];
+$c=$_GET["cnum"];
+$p=$_GET["pname"];
+$cause=$_GET["cause"];
+$b=$_GET["bnum"];
+$add=$_GET["adr"];
+$m=$_GET["mail"];
+$s=$_GET["sch"];
+$class=$_GET["class"];
+$size=$_GET["size"];
+$meat=$_GET["meat"];
+$e=$_GET["eat"];
+$y=$_GET["yobin"];
+$w=$_GET["way"];
+$back=$_GET["back"];
+$how=$_GET["Name1"];
+$o=$_GET["other"];
+$all=$_GET["Name2"];
+$medal=$_GET["medal"];
+$main=$_GET["main"];
+$q=$_GET["Q"];
+$f=$_GET["fart"];
+
+echo "<br>"."你的名字是:".$Name."<br/>";
+echo "你的性別是:".$gender."<br/>";
+echo "你的生日是:".$birth."<br/>";
+echo "你的身分證字號是:".$id."<br/>";
+echo "你的家裡電話是:".$h."<br/>";
+echo "您的手機是:".$c."<br/>";
+echo "你的老大名子是:".$p."<br/>";
+echo "跟你有啥關係:".$cause."<br/>";
+echo "你老大電話或手機:".$b."<br/>";
+echo "你現在住哪:".$add."<br/>";
+echo "電子信箱:".$m."<br/>";
+echo "就讀何所高中職:".$s."<br/>";
+echo "班級:".$class."<br/>";
+echo "你多月半啊:".$size."<br/>";
+echo "吃不吃肉:".$meat."<br/>";
+echo "特殊飲食習慣:".$e."<br/>";
+echo "有沒有甚麼毛病:".$y."<br/>";
+echo "怎麼來參加鼻地營:".$w."<br/>";
+echo "怎麼來回家R:".$back."<br/>";
+echo "你怎麼知道有鼻地營的:".$how."<br/>";
+echo "其他是啥:".$o."<br/>";
+echo "是否全程參與營隊:".$all."<br/>";
+echo "牌位:".$medal."<br/>";
+echo "main角:".$main."<br/>";
+echo "你還有什麼問題嗎???:".$q."<br/>";
+echo "有屁快放:".$f."<br/>";
+
+
+
+
+
+
+
+?>
+</center>
+</br></br>
+<center><b><HR width="50%"></HR></b></center>
+</br>
+</form>
+<center><B>╰⊙-⊙╮感謝乾爹報名╭⊙-⊙╯!</B></center>
+
+</body>
+</html>
